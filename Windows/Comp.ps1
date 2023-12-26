a742c41d159976519eb204046411ead7ad32d043
Altoid0 <tanay.v.shah@gmail.com>
2023-12-25 22:27:05 -0800

    Shares

--- Windows/Comp.ps1
+++ Windows/Comp.ps1
@@ -1,16 +1,14 @@
 $events = Get-WinEvent -FilterHashtable @{LogName='System';ID=7045}
 
-    Write-Output "#########################"
-    Write-Output "#    Service Creation   #"
-    Write-Output "#########################"
+Write-Output "#########################"
+Write-Output "#    Service Creation   #"
+Write-Output "#########################"
 
 foreach ($event in $events) {
     $properties = $event.Properties
-
     $creationDate = $event.TimeCreated
     $serviceName = $properties[0].Value
     $binaryPath = $properties[1].Value
-
     Write-Output "Creation Date: $creationDate"
     Write-Output "Service Name: $serviceName"
     Write-Output "Binary Path: $binaryPath"
@@ -25,11 +23,30 @@ Write-Output "#########################"
 
 foreach ($event in $events) {
     $properties = $event.Properties
-
     $creationDate = $event.TimeCreated
-
     Write-Output "Computer Name: $($properties[1].Value)"
     Write-Output "User: $($properties[5].Value)"
     Write-Output "Event Time: $creationDate"
     Write-Output ""
+}
+
+Write-Output "#########################"
+Write-Output "#     Share Access      #"
+Write-Output "#########################"
+
+$events = Get-WinEvent -FilterHashtable @{LogName='Security';ID=5140}
+foreach ($event in $events) {
+    if ($event.Properties[7].Value -match "IPC") {
+        $properties = $event.Properties
+
+        $creationDate = $event.TimeCreated
+
+        Write-Output "Share: $($properties[7].Value)"
+        Write-Output "Username: $($properties[1].Value)"
+        Write-Output "Ip Address: $($properties[5].Value)"
+        Write-Output "Access Mask: $($properties[9].Value)"
+        Write-Output "Event Time: $creationDate"
+        Write-Output ""
+    }
+    
 }
