c784e2b9cef657247322289e9218011efc93ad1b
Altoid0 <tanay.v.shah@gmail.com>
2024-01-18 23:09:52 -0800

    headers

--- Windows/Comp.ps1
+++ Windows/Comp.ps1
@@ -1,3 +1,22 @@
+$Error.Clear()
+$ErrorActionPreference = "SilentlyContinue"
+
+Write-Output "#########################"
+Write-Output "#                       #"
+Write-Output "#          Comp         #"
+Write-Output "#                       #"
+Write-Output "#########################"
+
+
+Write-Output "#########################"
+Write-Output "#    Hostname/Domain    #"
+Write-Output "#########################"
+Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | Select-Object Name, Domain
+Write-Output "#########################"
+Write-Output "#          IP           #"
+Write-Output "#########################"
+Get-WmiObject Win32_NetworkAdapterConfiguration | ? {$_.IpAddress -ne $null} | % {$_.ServiceName + "`n" + $_.IPAddress + "`n"}
+
 $events = Get-WinEvent -FilterHashtable @{LogName='System';ID=7045}
 
 Write-Output "#########################"
